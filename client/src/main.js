a1d8ba535b4a24316b03f7ec6f2f299e41f98c56
Kedr123 <48621337+Kedr123@users.noreply.github.com>
2023-04-22 15:48:04 +0300

    Подключение stor в проект

--- client/src/main.js
+++ client/src/main.js
@@ -1,7 +1,9 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from "@/router/router";
+import {store} from "core-js/internals/reflect-metadata";
 
 createApp(App)
     .use(router)
+    .use(store)
     .mount('#app')
