e6c779592bb2d61ff647327801055a5550bf95f9
Kedr123 <48621337+Kedr123@users.noreply.github.com>
2023-04-22 15:38:33 +0300

    Подключение сертификата для https

--- client/vue.config.js
+++ client/vue.config.js
@@ -1,14 +1,13 @@
-const { defineConfig } = require('@vue/cli-service')
+const {defineConfig} = require('@vue/cli-service');
+const fs = require('fs');
 module.exports = defineConfig({
-  transpileDependencies: true,
-  devServer:{
-    https:false,
-    headers: {
-      'Access-Control-Allow-Origin': '*',
-      'Cache-Control': null,
-      'X-Requested-With': null,
-      'Content-Type': 'text/html'
+    transpileDependencies: true,
+    devServer: {
+        https: {
+            key: fs.readFileSync("./certs/localhost+2-key.pem"),
+            cert: fs.readFileSync('./certs/localhost+2.pem'),
+            url:"https://127.0.0.1:8080",
+        },
     }
-  },
 
 })
